ac3f1034a4098aca57a8e489094249ce5cde8a43
chenhuiChang <j4demacia@gmail.com>
2016-07-26 18:35:02 +0800

    add computer level: child

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,12 +2,39 @@ var itemArr = ['<i class="fa fa-circle-o fa-5x centerlize" aria-hidden="true"></
     item = 0,
     count = 0,
     blockArr = new Array(10),
-    i;
+    i,
+    computer = {};
 restart();
-function end() {
+computer.term = false;
+// mode: chlid, normal, master
+computer.mode = "chlid";
+computer.findDanger = function (enemy) {
+
+}
+computer.run = function () {
+    computer.term = false;
+    if (blockArr[5] === 5) {
+        chooseB(5);
+    } else if (this.mode === "chlid") {
+        for(i=1;i<blockArr.length;i++){
+            if (blockArr[i] === 5) {
+                chooseB(i);
+                break;
+            }
+        }
+    } else if (this.mode === "normal") {
+        var friend = item,
+            enemy = item ^ 1;
+        this.findDanger(enemy);
+    }
+}
+
+function next() {
     var row = new Array(3),
         col = new Array(3),
         cross = new Array(3);
+    item ^= 1;
+    count += 1;
     row[0] = blockArr[1] + blockArr[2] + blockArr[3];
     row[1] = blockArr[4] + blockArr[5] + blockArr[6];
     row[2] = blockArr[7] + blockArr[8] + blockArr[9];
@@ -38,34 +65,39 @@ function end() {
     } else if (count === 9) {
         alert('Tie');
         restart();
+    } else if (computer.term) {
+        computer.run();
     }
 }
-function next() {
-    item ^= 1;
-    count += 1;
-    end();
-}
 function init() {
-    $('#b1').css({'height':$('#b1').width()+'px'}).css({'border-top':'none'}).css({'border-left':'none'});
-    $('#b2').css({'height':$('#b2').width()+'px'}).css({'border-top':'none'});
-    $('#b3').css({'height':$('#b3').width()+'px'}).css({'border-top':'none'}).css({'border-right':'none'});
-    $('#b4').css({'height':$('#b4').width()+'px'}).css({'border-left':'none'});
-    $('#b5').css({'height':$('#b5').width()+'px'});
-    $('#b6').css({'height':$('#b6').width()+'px'}).css({'border-right':'none'});
-    $('#b7').css({'height':$('#b7').width()+'px'}).css({'border-bottom':'none'}).css({'border-left':'none'});
-    $('#b8').css({'height':$('#b8').width()+'px'}).css({'border-bottom':'none'});
-    $('#b9').css({'height':$('#b9').width()+'px'}).css({'border-bottom':'none'}).css({'border-right':'none'});
-    
-    $('#b1').click(function () { if (blockArr[1] === 5) { $('#b1').append(itemArr[item]); blockArr[1] = item; next(); } });
-    $('#b2').click(function () { if (blockArr[2] === 5) { $('#b2').append(itemArr[item]); blockArr[2] = item; next(); } });
-    $('#b3').click(function () { if (blockArr[3] === 5) { $('#b3').append(itemArr[item]); blockArr[3] = item; next(); } });
-    $('#b4').click(function () { if (blockArr[4] === 5) { $('#b4').append(itemArr[item]); blockArr[4] = item; next(); } });
-    $('#b5').click(function () { if (blockArr[5] === 5) { $('#b5').append(itemArr[item]); blockArr[5] = item; next(); } });
-    $('#b6').click(function () { if (blockArr[6] === 5) { $('#b6').append(itemArr[item]); blockArr[6] = item; next(); } });
-    $('#b7').click(function () { if (blockArr[7] === 5) { $('#b7').append(itemArr[item]); blockArr[7] = item; next(); } });
-    $('#b8').click(function () { if (blockArr[8] === 5) { $('#b8').append(itemArr[item]); blockArr[8] = item; next(); } });
-    $('#b9').click(function () { if (blockArr[9] === 5) { $('#b9').append(itemArr[item]); blockArr[9] = item; next(); } });
+    $('#b1').css({ 'height': $('#b1').width() + 'px' }).css({ 'border-top': 'none' }).css({ 'border-left': 'none' });
+    $('#b2').css({ 'height': $('#b2').width() + 'px' }).css({ 'border-top': 'none' });
+    $('#b3').css({ 'height': $('#b3').width() + 'px' }).css({ 'border-top': 'none' }).css({ 'border-right': 'none' });
+    $('#b4').css({ 'height': $('#b4').width() + 'px' }).css({ 'border-left': 'none' });
+    $('#b5').css({ 'height': $('#b5').width() + 'px' });
+    $('#b6').css({ 'height': $('#b6').width() + 'px' }).css({ 'border-right': 'none' });
+    $('#b7').css({ 'height': $('#b7').width() + 'px' }).css({ 'border-bottom': 'none' }).css({ 'border-left': 'none' });
+    $('#b8').css({ 'height': $('#b8').width() + 'px' }).css({ 'border-bottom': 'none' });
+    $('#b9').css({ 'height': $('#b9').width() + 'px' }).css({ 'border-bottom': 'none' }).css({ 'border-right': 'none' });
+
+    $('#b1').click(function () { computer.term = true; chooseB(1); });
+    $('#b2').click(function () { computer.term = true; chooseB(2); });
+    $('#b3').click(function () { computer.term = true; chooseB(3); });
+    $('#b4').click(function () { computer.term = true; chooseB(4); });
+    $('#b5').click(function () { computer.term = true; chooseB(5); });
+    $('#b6').click(function () { computer.term = true; chooseB(6); });
+    $('#b7').click(function () { computer.term = true; chooseB(7); });
+    $('#b8').click(function () { computer.term = true; chooseB(8); });
+    $('#b9').click(function () { computer.term = true; chooseB(9); });
     $('#newGame').click(restart);
+}
+function chooseB(n) {
+    var str = "#b" + n;
+    if (blockArr[n] === 5) {
+        $(str).append(itemArr[item]);
+        blockArr[n] = item;
+        next();
+    }
 }
 function restart() {
     $('#b1').html('');
